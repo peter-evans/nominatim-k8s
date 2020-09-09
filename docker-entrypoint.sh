13585ff9de5c3d2272595bf9e9db0c1106a2dbd6
Joshua Jackson <joshua.jackson@jobvite-inc.com>
2020-09-09 16:03:07 -0700

    Rewrite the kubernetes charts to not require multiple steps
    Why do we need this change?
    =======================
    The documentation talks about launching one container then after it
    completes launching another. Instead of doing that leverage the ability
    to run initcontainers that run before the main application. As we only
    have one base container we need to extend logic in the docker-entrypoint
    to support these changes
    
    What effects does this change have?
    =======================
    * Change docker-entrypoint.sh to a modular function based design
    ** Initialize a db if location is null, so that it can eventually be
    externalized via a PVC/other means
    * Deletes: kubernetes/nominatim-canary.yaml kubernetes/nominatim.yaml
    * Adds a full chart definition that will work for things like helm
    * Add default values.yaml
    * Creates a deployment with init containers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,77 +9,103 @@ fi
 NOMINATIM_DATA_PATH=${NOMINATIM_DATA_PATH:="/srv/nominatim/data"}
 NOMINATIM_DATA_LABEL=${NOMINATIM_DATA_LABEL:="data"}
 NOMINATIM_PBF_URL=${NOMINATIM_PBF_URL:="http://download.geofabrik.de/asia/maldives-latest.osm.pbf"}
-NOMINATIM_POSTGRESQL_DATA_PATH=${NOMINATIM_POSTGRESQL_DATA_PATH:="/var/lib/postgresql/9.5/main"}
+NOMINATIM_POSTGRESQL_DATA_PATH=${NOMINATIM_POSTGRESQL_DATA_PATH:="/var/lib/postgresql/9.5/data"}
 # Google Storage variables
 NOMINATIM_SA_KEY_PATH=${NOMINATIM_SA_KEY_PATH:=""}
 NOMINATIM_PROJECT_ID=${NOMINATIM_PROJECT_ID:=""}
 NOMINATIM_GS_BUCKET=${NOMINATIM_GS_BUCKET:=""}
 NOMINATIM_PG_THREADS=${NOMINATIM_PG_THREADS:=2}
 
-if [ "$NOMINATIM_MODE" == "CREATE" ]; then
-    
+download_pbf() {
     # Retrieve the PBF file
     curl -L $NOMINATIM_PBF_URL --create-dirs -o $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.osm.pbf
     # Allow user accounts read access to the data
     chmod 755 $NOMINATIM_DATA_PATH
+}
 
-    # Start PostgreSQL
-    service postgresql start
+local_postgres_service_control() {
+  # Controls state of the local postges instance
+  state=$1
+  service postgresql "${state}"
+}
 
-    # Import data
+postgres_datadir() {
+  # run initdb if we don't have good contents
+  sed -i "s:data_directory.*:data_directory = '${NOMINATIM_POSTGRESQL_DATA_PATH}':" /etc/postgresql/9.5/main/postgresql.conf
+  if [ ! -f ${NOMINATIM_POSTGRESQL_DATA_PATH}/PG_VERSION ]
+  then
+    sudo -u postgres /usr/lib/postgresql/9.5/bin/initdb -D ${NOMINATIM_POSTGRESQL_DATA_PATH}
+  fi
+}
+
+postgres_initial_setup() {
+    # Add/create users
     sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim
     sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data
+    # drop the existing database if it exists
     sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim"
     useradd -m -p password1234 nominatim
+}
+
+nominatim_setup() {
+  # run setup with appropriate settings/files
     sudo -u nominatim /srv/nominatim/build/utils/setup.php --osm-file $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.osm.pbf --all --threads $NOMINATIM_PG_THREADS
+}
 
-    if [ ! -z "$NOMINATIM_SA_KEY_PATH" ] && [ ! -z "$NOMINATIM_PROJECT_ID" ] && [ ! -z "$NOMINATIM_GS_BUCKET" ]; then
-    
-        # Stop PostgreSQL
-        service postgresql stop
+google_cloud_setup(){
+  # activate settings to perform actions on google cloud
+  gcloud auth activate-service-account --key-file $NOMINATIM_SA_KEY_PATH
+  # Set the Google Cloud project ID
+  gcloud config set project $NOMINATIM_PROJECT_ID
+}
 
-        # Archive PostgreSQL data
-        tar cz $NOMINATIM_POSTGRESQL_DATA_PATH | split -b 1024MiB - $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.tgz_
+backup_local_db() {
+  # backup local db to upload to make for quick restores
+  tar cz $NOMINATIM_POSTGRESQL_DATA_PATH | split -b 1024MiB - $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.tgz_
+}
 
-        # Activate the service account to access storage
-        gcloud auth activate-service-account --key-file $NOMINATIM_SA_KEY_PATH
-        # Set the Google Cloud project ID
-        gcloud config set project $NOMINATIM_PROJECT_ID
+copy_google_bucket_data(){
+  source=$1
+  destination=$2
+  gsutil -m cp "${source}" "${destination}"
+}
 
-        # Copy the archive to storage
-        gsutil -m cp $NOMINATIM_DATA_PATH/*.tgz* $NOMINATIM_GS_BUCKET/$NOMINATIM_DATA_LABEL
-        
-        # Start PostgreSQL
-        service postgresql start
-        
-    fi
-    
+
+if [ "$NOMINATIM_MODE" == "CREATE" ]
+then
+  postgres_datadir
+  download_pbf
+  local_postgres_service_control start
+  postgres_initial_setup
+  nominatim_setup
+  if [ -n "$NOMINATIM_SA_KEY_PATH" ] && [ -n "$NOMINATIM_PROJECT_ID" ] && [ -n "$NOMINATIM_GS_BUCKET" ]
+  then
+    local_postgres_service_control stop
+    backup_local_db
+    google_cloud_setup
+    copy_google_bucket_data $NOMINATIM_DATA_PATH/*.tgz* $NOMINATIM_GS_BUCKET/$NOMINATIM_DATA_LABEL
+    local_postgres_service_control start
+  fi
+  # exit after initializing everything so main containers can start
+  exit 0
 else
+  if [ -n "$NOMINATIM_SA_KEY_PATH" ] && [ -n "$NOMINATIM_PROJECT_ID" ] && [ -n "$NOMINATIM_GS_BUCKET" ]
+  then
+    google_cloud_setup
+    mkdir -p ${NOMINATIM_DATA_PATH}
+    copy_google_bucket_data $NOMINATIM_GS_BUCKET/$NOMINATIM_DATA_LABEL/*.tgz* $NOMINATIM_DATA_PATH
+    # Remove any files present in the target directory
+    rm -rf ${NOMINATIM_POSTGRESQL_DATA_PATH:?}/*
 
-    if [ ! -z "$NOMINATIM_SA_KEY_PATH" ] && [ ! -z "$NOMINATIM_PROJECT_ID" ] && [ ! -z "$NOMINATIM_GS_BUCKET" ]; then
-    
-        # Activate the service account to access storage
-        gcloud auth activate-service-account --key-file $NOMINATIM_SA_KEY_PATH
-        # Set the Google Cloud project ID
-        gcloud config set project $NOMINATIM_PROJECT_ID
-
-        # Copy the archive from storage
-        mkdir -p $NOMINATIM_DATA_PATH
-        gsutil -m cp $NOMINATIM_GS_BUCKET/$NOMINATIM_DATA_LABEL/*.tgz* $NOMINATIM_DATA_PATH
-
-        # Remove any files present in the target directory
-        rm -rf $NOMINATIM_POSTGRESQL_DATA_PATH/*
-        
-        # Extract the archive
-        cat $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.tgz_* | tar xz -C $NOMINATIM_POSTGRESQL_DATA_PATH --strip-components=5
-        
-        # Start PostgreSQL
-        service postgresql start
-        
-    fi
-    
+    # Extract the archive
+    cat $NOMINATIM_DATA_PATH/$NOMINATIM_DATA_LABEL.tgz_* | tar xz -C $NOMINATIM_POSTGRESQL_DATA_PATH --strip-components=5
+    local_postgres_service_control start
+  fi
 fi
 
+# start postgres locally
+postgres_datadir
+local_postgres_service_control start
 # Tail Apache logs
 tail -f /var/log/apache2/* &
 
